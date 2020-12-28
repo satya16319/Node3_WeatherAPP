878ab8f07adb7ce7508d447e4a74cb49d3714718
satyajeet kumar <satyajeetkumar499@gmail.com>
2020-12-29 00:35:24 +0530

    init commit

---
+++ public/JS/index.js
@@ -0,0 +1,54 @@
+// console.log('kgkkkg');
+
+// const { response } = require("express");
+
+
+const weather=document.querySelector('form');
+const search=document.querySelector('input');
+// const message=ducument.querySelector('#first-1');
+const p=document.getElementById("first-1").innerHTML;
+// message.textContent="gjkgk";
+
+weather.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    // console.log(search.value);
+    document.getElementById("first-1").innerHTML='Loding.....';
+    const city=search.value;
+    if(!city)
+    {
+         console.log('City name can not empty');
+         document.getElementById("first-1").innerHTML='City name can not empty';
+    }
+    else
+    {
+          fetch(`http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=${city}`).then((response)=>{
+            response.json().then((data=>{
+                console.log(data.error.info);
+                if(data.error)
+                {
+                  // console.log(data);
+                   var gg=data.error.info;
+                   document.getElementById("first-1").innerHTML=gg;
+                }
+                else
+                {
+                   // console.log(data);
+                    document.getElementById("first-1").innerHTML=data.current.temperature;
+                }
+            }))
+          });
+         // console.log(data);
+        // const request=require('request');
+        // const url='http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=delhi'
+        
+        // request({url:url, json:true},(error,response)=>{
+        //     // const data=JSON.parse(response.body);
+        //     const data=response.body;
+        //      console.log(data);
+        // })
+         
+    }
+    // console.log('trt');
+    
+})
+console.log(weather);
---
+++ src/app.js
@@ -0,0 +1,64 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+const hbs=require('hbs');
+// console.log(__dirname);
+// console.log(path.join(__dirname,'..'));
+const publicdirectpath=path.join(__dirname,'../public');
+const viewpath=path.join(__dirname,'../template/viwes');
+const partialspath=path.join(__dirname,'../template/partials');
+
+app.use(express.static(publicdirectpath));
+app.set('view engine' ,'hbs');
+app.set('views',viewpath);
+hbs.registerPartials(partialspath);
+app.get('',(req,res)=>{
+    res.render('index',{
+        name:'satya',
+        title:'weather application',
+    });
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        name:'satya',
+        title:'about',
+    });
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        name:'satya',
+        title:'help',
+    });
+})
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+    return res.send('Error address dg');
+
+    res.send({
+        address:req.query.address,
+    })
+ })
+ 
+ app.get('/products',(req,res)=>{
+     console.log(req.query);
+     res.send({
+         product:[],
+     });
+  })
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'page not found'
+    })
+    // res.send('page 404');
+})
+
+//  app.get('/about',(req,res)=>{
+//     res.send('help express');
+//  })
+
+app.listen(3001,()=>{
+    console.log('server started');
+})
