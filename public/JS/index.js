40f4a4b182353c07db21c4ee0ea6bba334df010e
satyajeet kumar <satyajeetkumar499@gmail.com>
2021-03-18 19:17:44 +0530

    commit

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,57 +1,67 @@
 
 
+document.getElementById("f1").innerHTML="kk";
+var run=66;
+var totalval=43;
+var avg=((run/totalball)*6);
+var wicket=1;
+var over=7.1;
+var sc="UPH-"+run+"/"+wicket+ "("+over+")";
+console.log(sc);
+// document.getElementById("f1").innerHTML=67;
+
 // const { response } = require("express");
 
 
-const weather=document.querySelector('form');
-const search=document.querySelector('input');
-// const message=ducument.querySelector('#first-1');
-const p=document.getElementById("first-1").innerHTML;
-// message.textContent="gjkgk";
+// const weather=document.querySelector('form');
+// const search=document.querySelector('input');
+// // const message=ducument.querySelector('#first-1');
+// const p=document.getElementById("first-1").innerHTML;
+// // message.textContent="gjkgk";
 
-weather.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    // console.log(search.value);
-    document.getElementById("first-1").innerHTML='Loding.....';
-    const city=search.value;
-    if(!city)
-    {
-         console.log('City name can not empty');
-         document.getElementById("first-1").innerHTML='City name can not empty';
-    }
-    else
-    {
-      document.getElementById("first-1").innerHTML='Hi  we are going to deploy';
-          fetch(`http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=${city}`).then((response)=>{
-            response.json().then((data=>{
-                // console.log(data.error.info);
-                if(data.error)
-                {
-                  // console.log(data);
-                  document.getElementById("first-1").innerHTML='Hi  we have got some errors';
+// weather.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+//     // console.log(search.value);
+//     document.getElementById("first-1").innerHTML='Loding.....';
+//     const city=search.value;
+//     if(!city)
+//     {
+//          console.log('City name can not empty');
+//          document.getElementById("first-1").innerHTML='City name can not empty';
+//     }
+//     else
+//     {
+//       document.getElementById("first-1").innerHTML='Hi  we are going to deploy';
+//           fetch(`http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=${city}`).then((response)=>{
+//             response.json().then((data=>{
+//                 // console.log(data.error.info);
+//                 if(data.error)
+//                 {
+//                   // console.log(data);
+//                   document.getElementById("first-1").innerHTML='Hi  we have got some errors';
 
-                   var gg=data.error.info;
-                   document.getElementById("first-1").innerHTML=gg;
-                }
-                else
-                {
-                   // console.log(data);
-                    document.getElementById("first-1").innerHTML=data.current.temperature;
-                }
-            }))
-          });
-         // console.log(data);
-        // const request=require('request');
-        // const url='http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=delhi'
+//                    var gg=data.error.info;
+//                    document.getElementById("first-1").innerHTML=gg;
+//                 }
+//                 else
+//                 {
+//                    // console.log(data);
+//                     document.getElementById("first-1").innerHTML=data.current.temperature;
+//                 }
+//             }))
+//           });
+//          // console.log(data);
+//         // const request=require('request');
+//         // const url='http://api.weatherstack.com/current?access_key=061ca3d6ebdc9d876fb75f2a4e0abfdd&query=delhi'
         
-        // request({url:url, json:true},(error,response)=>{
-        //     // const data=JSON.parse(response.body);
-        //     const data=response.body;
-        //      console.log(data);
-        // })
+//         // request({url:url, json:true},(error,response)=>{
+//         //     // const data=JSON.parse(response.body);
+//         //     const data=response.body;
+//         //      console.log(data);
+//         // })
          
-    }
-    // console.log('trt');
+//     }
+//     // console.log('trt');
     
-})
-console.log(weather);
+// })
+// console.log(weather);
